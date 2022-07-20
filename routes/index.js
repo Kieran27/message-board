252cd0811a5ec089c8a32d0dcb352d12f471027a
Kieran Singh <kieransingh27@gmail.com>
2022-07-20 15:10:24 +1000

    Message board app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+router.post('/new', (req, res) => {
+  const reqBody = req.body;
+  const message = {
+    text: reqBody.message,
+    user: reqBody.user,
+    added: new Date()
+  }
+  messages.push(message);
+  res.redirect('/');
+})
 
 module.exports = router;
